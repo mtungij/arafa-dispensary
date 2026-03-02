468749d3ff1d543348d963a0661389599014adac
mtungij <jamesmtungi2013@gmail.com>
2026-03-02 21:39:51 +0300

    hello

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -8,6 +8,7 @@
               content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token"
               content="{{ csrf_token() }}">
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title> {{ config('app.name') }} {{ isset($title) ? '| ' . $title : '' }}</title>
         <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
 
